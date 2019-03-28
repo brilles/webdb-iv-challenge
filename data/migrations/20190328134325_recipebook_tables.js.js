ac9f658a3e3897cfcbd106c467697aca9198f373
Brian Illes <brianrilles@gmail.com>
2019-03-28 14:24:32 -0700

    all tables complete

---
+++ data/migrations/20190328134325_recipebook_tables.js.js
@@ -0,0 +1,53 @@
+exports.up = function(knex, Promise) {
+  return knex.schema
+    .createTable('dishes', tbl => {
+      tbl.increments();
+      tbl.string('name', 128).notNullable();
+    })
+    .createTable('recipes', tbl => {
+      tbl.increments();
+      tbl
+        .string('name', 128)
+        .notNullable()
+        .unique();
+      tbl
+        .integer('dish_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('dishes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+    })
+    .createTable('ingredients', tbl => {
+      tbl.increments();
+      tbl.string('name', 128).notNullable();
+    })
+    .createTable('recipe_ingredients', tbl => {
+      tbl.increments();
+      tbl
+        .integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('recipes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      tbl
+        .integer('ingredients_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('ingredients')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+    });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+    .dropTableIfExists('recipe_ingredients')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes')
+    .dropTableIfExists('dishes');
+};
